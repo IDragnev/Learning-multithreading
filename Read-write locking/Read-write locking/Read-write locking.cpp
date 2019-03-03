bc9545386693332cae78e8e6a5c6d357dad24f5d
IDragnev <iddragnev@gmail.com>
2019-03-03 19:34:11 +0200

    Make Read-write locking implementation more complete
    Added a mutate-enabled range view to the example. This needs
    a unique_lock in order to the lock being transfered to the view
    object and the mutex being released with its destruction.

--- Read-write locking/Read-write locking/Read-write locking.cpp
+++ Read-write locking/Read-write locking/Read-write locking.cpp
@@ -4,14 +4,23 @@
 #include <shared_mutex>
 #include <vector>
 #include <functional>
+#include <cassert>
 #include "..\..\Parallel Accumulate\Parallel Accumulate\SmartThread.h"
 
 using IDragnev::Threads::SmartThread;
+
+using Predicate = std::function<bool(int)>;
 
 struct Entry 
 {
 	std::string value = "null";
 };
+
+std::ostream& operator<<(std::ostream& out, const Entry& e)
+{
+	out << e.value.c_str();
+	return out;
+}
 
 class Cache
 {
@@ -20,40 +29,68 @@ private:
 	using Map = std::unordered_map<Key, Entry>;
 	using Pair = Map::value_type;
 
-	using Predicate = std::function<bool(int)>;
-
 	using WriterLock = std::lock_guard<std::shared_mutex>;
 	using ReaderLock = std::shared_lock<std::shared_mutex>;
 
 public:
-	class RangeView
+	class ImmutableRangeView
 	{
 	private:
 		friend class Cache;
-
 		using ConstEntryRef = std::reference_wrapper<const Entry>;
 		using Range = std::vector<ConstEntryRef>;
+		using Lock = ReaderLock;
 
 	public:
 		auto begin() const { return range.begin(); }
 		auto end() const { return range.end(); }
 
 	private:
-		RangeView(ReaderLock&& lock, Range&& range) :
+		ImmutableRangeView(Lock&& lock, Range&& range) :
 			lock(std::move(lock)), 
 			range(std::move(range))
 		{
+			assert(this->lock.owns_lock());
 		}
 
 	private:
-		ReaderLock lock;
+		Lock lock;
+		Range range;
+	};
+
+	class RangeView
+	{
+	private:
+		friend class Cache;
+		using EntryRef = std::reference_wrapper<Entry>;
+		using Range = std::vector<EntryRef>;
+		using Lock = std::unique_lock<std::shared_mutex>;
+
+	public:
+		auto begin() { return range.begin(); }
+		auto end() { return range.end(); }
+
+	private:
+		RangeView(Lock&& lock, Range&& range) :
+			lock(std::move(lock)),
+			range(std::move(range))
+		{
+			assert(this->lock.owns_lock());
+		}
+
+	private:
+		Lock lock;
 		Range range;
 	};
 
 public:
 	Cache(std::initializer_list<Pair> pairs) : map(pairs) { }
-	
-	void updateEntry(const Key& key, const Entry& entry)
+	~Cache() = default;
+
+	Cache(const Cache&) = delete;
+	Cache& operator=(const Cache&) = delete;
+
+ 	void updateEntry(const Key& key, const Entry& entry)
 	{
 		auto lock = WriterLock(mutex);
 		map[key] = entry;
@@ -66,20 +103,35 @@ public:
 		return (iterator != map.cend()) ? iterator->second : Entry{};
 	}
 
-	auto viewAll(Predicate p) const
+	auto viewAllReadOnly(Predicate p) const
 	{
-		auto lock = ReaderLock(mutex);
-		auto range = RangeView::Range{};
+		return makeView<ImmutableRangeView>(mutex, map, p);
+	}
 
-		for (const auto& [key, entry] : map)
+	auto viewAll(Predicate p)
+	{
+		return makeView<RangeView>(mutex, map, p);
+	}
+
+private:
+	template <typename View, typename Map>
+	static auto makeView(std::shared_mutex& mutex, Map& map, Predicate p)
+	{
+		using Lock = typename View::Lock;
+		using Range = typename View::Range;
+
+		auto lock = Lock(mutex);
+		auto range = Range{};
+
+		for (auto&[key, entry] : map)
 		{
 			if (p(key))
-			{ 
+			{
 				range.emplace_back(entry);
 			}
 		}
 
-		return RangeView(std::move(lock), std::move(range));
+		return View(std::move(lock), std::move(range));
 	}
 
 private:
@@ -107,17 +159,17 @@ auto joinAll = [](auto& threads)
 
 int main()
 {
-	Cache cache{ { 1, {"e1"} }, { 2, {"e2"} }, { 3, {"e3"} } };
+	Cache cache{ { 1, {"e1"} }, { 2, {"e2"} }, { 3, {"e3"} }, { 4, {"e4"} } };
 	auto threads = launchReaders(cache, { 1, 2, 3, 4, 5, 6 });
 
 	cache.updateEntry(111, { "e4" });
-	auto range = cache.viewAll([](int key) { return key % 2 == 0; });
+	auto range = cache.viewAllReadOnly([](int key) { return key % 2 == 0; });
 	
 	joinAll(threads);
 
 	for (const Entry& entry : range)
 	{
-		std::cout << entry.value.c_str() << "\n";
+		std::cout << entry << "\n";
 	}
 
 	return 0;
