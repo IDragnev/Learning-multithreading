c5595087d28108d829719ca9b03d5067d22ffc9e
IDragnev <iddragnev@gmail.com>
2019-03-28 22:21:01 +0200

    Add a non-atomic variable to the acquire-release example

--- Memory orderings/Memory orderings/Transitive synchronization with acquire-release.cpp
+++ Memory orderings/Memory orderings/Transitive synchronization with acquire-release.cpp
@@ -4,6 +4,7 @@
 #include <assert.h>
 #include "..\..\Parallel Accumulate\Parallel Accumulate\SmartThread.h"
 
+using namespace std::chrono_literals;
 using IDragnev::Threads::SmartThread;
 
 enum class DataStates : std::uint8_t
@@ -13,11 +14,15 @@ enum class DataStates : std::uint8_t
 	ready
 };
 
+int nonAtomic = 0;
 std::atomic<int> data = 0;
 std::atomic<DataStates> state = DataStates::raw;
 
 void initializeData()
 {
+	std::this_thread::sleep_for(2s);
+
+	nonAtomic = 100;
 	data.store(123, std::memory_order_relaxed);
 	state.store(DataStates::initialized, std::memory_order_release);
 }
@@ -38,6 +43,7 @@ void processData()
 {
 	while (state.load(std::memory_order_acquire) != DataStates::ready)
 	{ }
+	assert(nonAtomic == 100);
 	assert(data.load(std::memory_order_relaxed) == 123);
 }
 
